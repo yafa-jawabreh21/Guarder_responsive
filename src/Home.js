47aa1bedc097045c7dbcd71205eac1245d9e86a3
yafa jawabreh <217853@ppu.edu.ps>
2024-10-21 18:08:48 +0300

    add homesection

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
+import Quarder from "./components/Quarder";
 function Home () {
     return(
         <>
         
 
-        <div className="bg-cover bg-no-repeat bg-[url('https://img.freepik.com/free-photo/portrait-male-security-guard-with-radio-station_23-2150368765.jpg?w=1060&t=st=1729344618~exp=1729345218~hmac=2e4e5cfafcd172681dbec1ddbd7275a5977b2890efb10625b0b2d7edc572f260')]" >
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 pl-10 ">
+        <div className="bg-cover bg-no-repeat bg-[url('https://xpressguards.com/wp-content/uploads/2020/10/top-rated-security-guards.jpg')]" >
+        <nav class="  dark:bg-gray-900 pl-10 ">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        {/* <img src="" class="h-8" alt="Flowbite Logo" /> */}
+        
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><span className="text-yellow-300">G</span>uarder</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -16,27 +17,28 @@ function Home () {
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:hover:bg-black md:bg-opacity-80 md:p-3 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+    <div class="hidden w-full md:block md:w-auto md:pr-36" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li className="bg-black px-2 rounded-md">
+          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:hover:bg-black md:bg-opacity-80 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:bg-black md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        <li className="md:hover:bg-black md:hover:rounded-sm">
+          <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:bg-black md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ">About</a>
         </li>
-        <li>
+        <li className="md:hover:bg-black md:hover:rounded-sm">
           <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:bg-black md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
         </li>
-        <li>
+        <li className="md:hover:bg-black md:hover:rounded-sm">
           <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:bg-black md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Quarder</a>
         </li>
-        <li>
+        <li className="md:hover:bg-black md:hover:rounded-sm">
           <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:bg-black md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+<Quarder/>
 </div>
 
 
