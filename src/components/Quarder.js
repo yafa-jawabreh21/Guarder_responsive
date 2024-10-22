1d7ce0e5d009398657fea96b81152aed9b95b5a9
yafa jawabreh <217853@ppu.edu.ps>
2024-10-22 17:09:40 +0300

    update Quarder component

--- src/components/Quarder.js
+++ src/components/Quarder.js
@@ -3,7 +3,7 @@ function Quarder () {
         <>
         <div className="block h-fit md:p-60 px-14 py-20">
             <h1 className="text-white font-bold md:text-2xl text-left lett tracking-widest">YOUR SAFTEY <br></br><span className="text-yellow-300">OUR RESPONSIBILITY</span></h1><br></br>
-            <p className="text-white text-xs sm:text-base md:text-base text-left text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing <br></br>elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p className="text-white text-xs sm:text-sm md:text-sm text-left text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing <br></br>elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
             <div className="md:flex md:gap-3 py-6 float-left">
             <button className="bg-white text-black py-1 px-3 font-medium rounded-sm mr-3">Read More</button>
             <button className="bg-yellow-300 text-white py-1 px-3 font-medium rounded-sm">Get Qquote</button>
