b41b8c2a4a13976ca1f39219adfc8572ece99921
yafa jawabreh <217853@ppu.edu.ps>
2024-10-23 17:21:32 +0300

    .

--- src/components/Quarder.js
+++ src/components/Quarder.js
@@ -1,7 +1,7 @@
 function Quarder () {
     return(
         <>
-        <div className="block h-fit md:p-60 px-14 py-20">
+        <div className="block  md:p-60 px-14 py-20">
             <h1 className="text-white font-bold md:text-2xl text-left lett tracking-widest">YOUR SAFTEY <br></br><span className="text-yellow-300">OUR RESPONSIBILITY</span></h1><br></br>
             <p className="text-white text-xs sm:text-sm md:text-sm text-left text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing <br></br>elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
             <div className="md:flex md:gap-3 py-6 float-left">
