e9590236373ceb20f477dfef6be52a5c417afba1
yafa jawabreh <217853@ppu.edu.ps>
2024-10-25 20:59:10 +0300

    update services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,7 @@ function Services () {
         <>
         <div id="section2" className="bg-black opacity-90 text-white text-center p-7 ">
             <h1 className="text-white text-base sm:text-lg md:text-xl font-bold tracking-widest sm:mt-16 sm:mb-10 mb-5">OUR SERVICES</h1>
-            <div className="block md:flex md:gap-5   my-0 mx-40  text-black">
+            <div className="block md:flex md:gap-5   my-0 lg:mx-40  text-black">
                 <div data-aos="zoom-in" data-aos-offset="300" className="block p-10 bg-white mb-10">
                 <svg class="w-20 h-20 my-0 mx-auto text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
   <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
