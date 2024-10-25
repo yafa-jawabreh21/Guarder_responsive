12b75dfb592bba1715d5b26cbc4e7657ceec5e47
yafa jawabreh <217853@ppu.edu.ps>
2024-10-25 20:58:23 +0300

    add animation to components

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 function About () {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return(
         <>
-        <div id="section1" className="h-fit block sm:flex sm:gap-5  p-14">
-            <img className="sm:h-1/2 sm:w-1/2" src="https://absoluteisi.com/wp-content/uploads/2021/03/A5D0B9A5-8EB5-4046-9391-52B49432E1DB.jpeg" ></img>
-            <div className="bg-white p-10 sm:h-1/2 sm:w-1/2 ">
+        <div  id="section1" className="h-fit block sm:flex sm:gap-5  p-14">
+            <img data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" className="sm:h-1/2 sm:w-1/2" src="https://absoluteisi.com/wp-content/uploads/2021/03/A5D0B9A5-8EB5-4046-9391-52B49432E1DB.jpeg" ></img>
+            <div data-aos="fade-left" className="bg-white p-10 sm:h-1/2 sm:w-1/2 ">
                 <h1 className="font-extrabold text-xl tracking-widest pt-2 pb-4 text-left">WHO ARE WE?</h1>
                 <p className="text-left text-xs sm:text-xs md:text-base md:font-normal font-medium pb-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quisquam laboriosam adipisci cupiditate, eos ipsa placeat. Eius cupiditate aliquam minus omnis? Voluptas hic libero unde placeat facilis possimus quis animi.
--- src/components/Guards.js
+++ src/components/Guards.js
@@ -1,4 +1,10 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 function Quards() {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <>
             <div id="section3">
@@ -8,19 +14,19 @@ function Quards() {
                 </p>
                 <div className="md:flex  justify-center md:justify-start md:gap-1 md:mx-5">
                     {/* Each guard's card */}
-                    <div className="relative my-4 mx-2 h-64 w-full md:w-1/3  bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714682/pexels-photo-7714682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+                    <div data-aos="zoom-in" data-aos-offset="300" className="relative my-4 mx-2 h-64 w-full md:w-1/3  bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714682/pexels-photo-7714682.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
                         <div className="absolute bottom-0 left-0 w-full bg-black opacity-70 text-white h-1/4 md:h-2/5 flex flex-col justify-center items-center">
                             <h2 className="text-white text-xl font-bold tracking-wide">Nathan Mcpherson</h2>
                             <h5 className="text-sm font-bold tracking-wide">Supervisor</h5>
                         </div>
                     </div>
-                    <div className="relative my-4 mx-2 h-64 w-full md:w-1/3 bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714709/pexels-photo-7714709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+                    <div data-aos="zoom-in" data-aos-offset="400" className="relative my-4 mx-2 h-64 w-full md:w-1/3 bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714709/pexels-photo-7714709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
                         <div className="absolute bottom-0 left-0 w-full bg-black opacity-70 text-white h-1/4 md:h-2/5 flex flex-col justify-center items-center">
                             <h2 className="text-white text-xl font-bold tracking-wide">Denny Bulter</h2>
                             <h5 className="text-sm font-bold tracking-wide">Supervisor</h5>
                         </div>
                     </div>
-                    <div className="relative my-4 mx-2 h-64 w-full md:w-1/3 bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714739/pexels-photo-7714739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+                    <div data-aos="zoom-in" data-aos-offset="500" className="relative my-4 mx-2 h-64 w-full md:w-1/3 bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/7714739/pexels-photo-7714739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
                         <div className="absolute bottom-0 left-0 w-full bg-black opacity-70 text-white h-1/4 md:h-2/5 flex flex-col justify-center items-center">
                             <h2 className="text-white text-xl font-bold tracking-wide">Keit Mcpherson</h2>
                             <h5 className="text-sm font-bold tracking-wide">Supervisor</h5>
@@ -28,7 +34,7 @@ function Quards() {
                     </div>
                     
                 </div>
-                <button className="bg-yellow-300 text-white py-1 px-10 mt-4 mb-10 font-medium rounded-sm hover:bg-yellow-400">View All</button>
+                <button data-aos="zoom-in" data-aos-offset="500" className="bg-yellow-300 text-white py-1 px-10 mt-4 mb-10 font-medium rounded-sm hover:bg-yellow-400">View All</button>
             </div>
         </>
     );
